3f99ee8e162fe5e01193c6765bd4fc371dd748ad
Shreshth Verma <shreshth@shreshthverma.me>
2022-05-10 16:45:41 +0530

    feat: added cloudinary upload function

--- functions/uploadToCloudinary.ts
+++ functions/uploadToCloudinary.ts
@@ -1,1 +1,9 @@
-import cloudinary from '../config/cloudinary'
+import cloudinary from '../config/cloudinary';
+
+const upload = async (img) => {
+    let image = await cloudinary.uploader.upload(img, { upload_preset: "elect.io", width: 170, crop: "scale" });
+    const imageUrl = image.secure_url;
+    return imageUrl;
+}
+
+export default upload;
