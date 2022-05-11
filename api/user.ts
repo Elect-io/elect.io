d7f3974ca6375a12109165377e1f344b7c70874b
Shreshth Verma <shreshth@shreshthverma.me>
2022-05-11 21:14:12 +0530

    feat: added forgot password feature

--- api/user.ts
+++ api/user.ts
@@ -10,8 +10,8 @@ const simpleAvatarGenerator = require('simple-avatar-generator');
 import Profile from '../models/profile';
 import Verify from '../models/verify';
 import mailjet from '../config/mailjet';
-import { signupEmail } from '../emailTemplates/authEmails';
-
+import { signupEmail, forgetEmail } from '../emailTemplates/authEmails';
+import ForgotPassword from '../models/forgotPassword';
 router.get('/', auth, async (req, res) => {
     try {
         console.log(req.user)
@@ -26,11 +26,112 @@ router.get('/', auth, async (req, res) => {
     }
 })
 
-router.get('/verify/:id', async (req, res)=>{
-    try{
+router.post('/forgot/:email', async (req, res) => {
+    try {
+
+        const user = await User.findOne({ email: req.params.email });
+        if (!user) {
+            return res.json({ msg: 'If an account is associated with this email address, we will send you an email regarding the password reset process.' })
+        }
+        let forgot = await ForgotPassword.findOne({ user: user._id });
+        if (!forgot) {
+            forgot = new ForgotPassword({ user: user._id });
+            await forgot.save();
+        }
+        let profile = await Profile.findOne({ user: user._id });
+        const Email = mailjet.post("send", { 'version': 'v3.1' })
+            .request({
+                "Messages": [{
+                    "From": {
+                        "Email": "shresth21oct@gmail.com",
+                        "Name": `Elect io`
+                    },
+                    "To": [{
+                        "Email": `${user.email}`,
+                        "Name": `${profile.name}`
+                    }],
+                    "Subject": `Verify Your Elect io Account`,
+                    "TextPart": "",
+                    "HTMLPart": forgetEmail(profile.name, forgot._id)
+                }]
+            });
+        await Email
+            .then(async a => {
+
+                return res.json({ msg: 'If an account is associated with this email address, we will send you an email regarding the password reset process.' })
+            }).catch(err => {
+                console.log(err);
+                return res.status(500).json({ error: "Our services are unavailable at this moment, please try again later!" })
+            })
+    }
+    catch (err) {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ error: "Not Found" });
+        }
+        return res.status(500).json({ error: "We can't process your request at this moment" });
+    }
+});
+router.post('/reset/:id', validator([{ name: 'password', minlength: 8 }]), async (req, res) => {
+    try {
+        const forgot = await ForgotPassword.findById(req.params.id);
+        if (!forgot) {
+            return res.status(404).json({ error: "Not Found" });
+        }
+        const user = await User.findById(forgot.user);
+        const salt = await bcrypt.genSalt(Number(process.env.salt_rounds))
+        const password = await bcrypt.hash(req.body.password, salt)
+        user.password = password;
+        await user.save();
+        const token = await generateJWT(user._id);
+        return res.json({ msg: "Password updated successfully!", token })
+    }
+    catch (err) {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ error: "Not Found" });
+        }
+        return res.status(500).json({ error: "We can't process your request at this moment" });
+    }
+});
+router.get('/forgot/:id', async (req, res) => {
+    try {
+        const forgot = await ForgotPassword.findById(req.params.id);
+        if (!forgot) {
+            return res.status(404).json({ error: "Not Found" });
+        }
+        const user = await User.findById(forgot.user).select('-password');
+        const profile = await Profile.findOne({ user: user._id })
+        return res.json({ forgot, user, profile });
+    }
+    catch (err) {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ error: "Not Found" });
+        }
+        return res.status(500).json({ error: "We can't process your request at this moment" });
+    }
+});
+
+router.get('/didn\'t-forget/:id', async (req, res) => {
+    try {
+        const forgot = await ForgotPassword.findById(req.params.id);
+        if (!forgot) {
+            res.redirect(`${process.env.client_url}/not-found`);
+        }
+        await forgot.delete();
+        return res.redirect(`${process.env.client_url}`);
+    }
+    catch (err) {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ error: "Not Found" });
+        }
+        return res.status(500).json({ error: "We can't process your request at this moment" });
+    }
+})
+
+router.get('/verify/:id', async (req, res) => {
+    try {
         const verify = await Verify.findById(req.params.id);
-        if(!verify){
-            return res.status(404).json({ error: "Not Found" });  
+        if (!verify) {
+            return res.status(404).json({ error: "Not Found" });
         }
         const user = await User.findById(verify.user);
         user.verified = true;
@@ -38,7 +139,7 @@ router.get('/verify/:id', async (req, res)=>{
         await verify.delete();
         return res.redirect(process.env.client_url);
     }
-    catch(err){
+    catch (err) {
         if (err.kind === 'ObjectId') {
             return res.status(404).json({ error: "Not Found" });
         }
@@ -101,7 +202,7 @@ router.post('/create-account',
             await Email
                 .then(async a => {
                     console.log(a.response);
-                    await verify         .save();
+                    await verify.save();
                     await profile.save();
                     await user.save();
                     return res.json({ token });
