73a94c050fde8bbcf480aadc6fd97cd8aa2bf7d6
Shreshth Verma <shreshth@shreshthverma.me>
2022-05-21 23:06:08 +0530

    feat: completed sidebar-toolkit

---
+++ client/src/components/reusables/sidebar-toolkit.jsx
@@ -0,0 +1,11 @@
+import {Link} from 'react-router-dom'
+const SideBarToolkit = (props) => {
+    return (<div className="sidebar-each-toolkit">
+        {props.options.map(option=>{
+            return(<Link className="sidebar-each-toolkit-each" to={option.link}>{option.title}</Link>)
+        })}
+        
+    </div>)
+}
+
+export default SideBarToolkit;
--- client/src/components/reusables/sidebar.jsx
+++ client/src/components/reusables/sidebar.jsx
@@ -4,6 +4,7 @@ import AccountCircle from '../../icons/account_circle_24px';
 import Group from '../../icons/Group';
 import Vector from '../../icons/Vector';
 import { Link } from 'react-router-dom'
+import SideBarToolkit from './sidebar-toolkit';
 
 class SideBar extends React.Component {
 
@@ -28,9 +29,9 @@ class SideBar extends React.Component {
     render() {
 
         return (<div className="sidebar">
-            <Link to="/profile"><AccountCircle onClick={() => this.setState({ selected: 1 })} active={this.state.selected === 1} /></Link>
-            <Link to="/home"  ><Vector onClick={() => this.setState({ selected: 2 })} active={this.state.selected === 2} /></Link>
-            <Link to="/elections" ><Group onClick={() => this.setState({ selected: 3 })} active={this.state.selected === 3} /></Link>
+            <Link to="/profile" className="sidebar-each"><AccountCircle onClick={() => this.setState({ selected: 1 })} active={this.state.selected === 1} /><SideBarToolkit options={[{link:"/login", title:"Login"}, {link:"/sign-up", title:"Sign Up"}]}/></Link>
+            <Link to="/home" className="sidebar-each"><Vector onClick={() => this.setState({ selected: 2 })} active={this.state.selected === 2} /></Link>
+            <Link to="/elections" className="sidebar-each"><Group onClick={() => this.setState({ selected: 3 })} active={this.state.selected === 3} /></Link>
 
         </div>)
     }
