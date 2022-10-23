36e9549382fdb8b8543181386dc36291fc554349
TheHamsterDog <shreshth@shreshthverma.me>
2022-10-23 18:21:19 +0530

    debugging

--- functions/uploadToCloudinary.ts
+++ functions/uploadToCloudinary.ts
@@ -1,8 +1,10 @@
 import cloudinary from '../config/cloudinary';
 
 const upload = async (img) => {
+    console.log("Starting Cloudinary Upload")
     let image = await cloudinary.uploader.upload(img, { upload_preset: "elect.io", width: 170, crop: "scale" });
     const imageUrl = image.secure_url;
+    console.log("Cloudinary Works")
     return imageUrl;
 }
 
